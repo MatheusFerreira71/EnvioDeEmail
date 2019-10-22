80d018321097bfd1301e6d6b7f08d034b9734bbe
Matheus Ferreira <matheusfernandofreire@hotmail.com>
2019-10-22 15:54:49 -0300

    Emails sendo enviados

--- index.js
+++ index.js
@@ -9,29 +9,63 @@ app.engine('handlebars', exphbars({
     extname: "handlebars",
     defaultLayout: "",
     layoutsDir: "",
- }));
+}));
+
 app.set('view engine', 'handlebars');
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.render('contact');
 });
 
-app.post('/send', (req, res) =>{
-    console.log(req.body)
-});
+
 
 const config = {
-    host: 'smtp.mailtrap.io',
-    port: 25,
+    host: 'smtp.alterdata.com.br',
+    port: 587,
+    secure: false,
     auth: {
-        user: '308cc2cf95a562',
-        pass: '4874696cdb437f'
+        user: 'matheus.franca@alterdata.com.br',
+        pass: 'Matheus#eu2'
     }
 };
-let transporter = nodeMailer.createTransport(config);
+
+app.post('/send', (req, res) => {
+    let transporter = nodeMailer.createTransport(config);
+
+    transporter.sendMail({
+        from: `Matheus Ferreira <matheus.franca@alterdata.com.br>`, // sender address
+        to: `${req.body.email}`, // list of receivers
+        subject: 'Hello ✔', // Subject line
+        text: `Segue uma cópia do seu feedback: ${req.body.mensagem}. 
+        A RM² Agradeçe seu contato!` // Plain text body
+    }, (error, info) => {
+        if (error) {
+            return console.log(error)
+        } else {
+            return console.log(info)
+        }
+    });
+
+    console.log(req.body.anexo)
+    transporter.sendMail({
+        from: `Matheus Ferreira <matheus.franca@alterdata.com.br>`, // sender address
+        to: `matheusfernandofreire@hotmail.com`, // list of receivers
+        subject: 'Hello ?', // Subject line
+        text: `Olá, o que tenho a dizer é: ${req.body.mensagem}! 
+    Meu E-mail é ${req.body.email} e meu nome é ${req.body.name} e estou a disposição para mais informações.
+        Obrigado!` // Plain text body
+    }, (error, info) => {
+        if (error) {
+            return console.log(error)
+        } else {
+            return console.log(info)
+        }
+    });
+});
+
 app.listen(3000, () => console.log('Server started...'));
